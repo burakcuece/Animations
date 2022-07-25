de371999b0a79ef59d706492a186ec9f6b983d8a
Burak Cüce <b.cuece@web.de>
2022-07-25 18:04:42 +0200

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Burak Cüce on 25.07.22.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Animations/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Animations
+//
+//  Created by Burak Cüce on 25.07.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
